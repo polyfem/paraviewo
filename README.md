81fb19b704a8b1c836468c8c449a7db8a9cb224c
teseo@sonic <teseo@sonic>
2023-02-05 12:32:01 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# paraviewo
+# ParaviewO
+
+A simple interface to export paraview files. Currently it supports `.vtu` and `.hdf`.
+
+The interface is in the `ParaviewWriter` class that provides
+
+```
+bool write_mesh(const std::string &path, const Eigen::MatrixXd &points, const Eigen::MatrixXi &cells)
+bool write_mesh(const std::string &path, const Eigen::MatrixXd &points, const std::vector<std::vector<int>> &cells, const bool is_simplicial, const bool has_poly)
+```
+to write a mesh (either as matrices of vertices and cells or as a vector of vector if the mesh is not uniform).
+
+The class supports adding fields
+```
+void add_field(const std::string &name, const Eigen::MatrixXd &data)
+```
+which *authomatically* writes a scalar or vector field. It also provides two specific methods (not recomended)
+```
+void add_scalar_field(const std::string &name, const Eigen::MatrixXd &data)
+void add_vector_field(const std::string &name, const Eigen::MatrixXd &data)
+```
+
+## Example Usage
+
+```
+ParaviewWriter writer = VTUWriter(); // or HDF5VTUWriter()
+writer.add_field("function", values);
+writer.write_mesh("out.vtu", v, f);
+```
---
+++ src/paraviewo/ParaviewWriter.hpp
@@ -0,0 +1,90 @@
+#pragma once
+
+#include <Eigen/Dense>
+
+namespace paraviewo
+{
+	class paraview_tags
+	{
+	private:
+		// https://vtk.org/doc/nightly/html/vtkCellType_8h_source.html#l00069
+		static const int VTK_VERTEX = 1;
+		static const int VTK_LINE = 3;
+		static const int VTK_TETRA = 10;
+		static const int VTK_TRIANGLE = 5;
+		static const int VTK_QUAD = 9;
+		static const int VTK_HEXAHEDRON = 12;
+		static const int VTK_POLYGON = 7;
+		static const int VTK_POLYHEDRON = 42;
+
+		static const int VTK_LAGRANGE_TRIANGLE = 69;
+		static const int VTK_LAGRANGE_QUADRILATERAL = 70;
+
+		static const int VTK_LAGRANGE_TETRAHEDRON = 71;
+		static const int VTK_LAGRANGE_HEXAHEDRON = 72;
+
+	public:
+		inline static int VTKTagVolume(const int n_vertices, bool is_simplex, bool is_poly)
+		{
+			switch (n_vertices)
+			{
+			case 1:
+				return VTK_VERTEX;
+			case 2:
+				return VTK_LINE;
+			case 3:
+				return VTK_TRIANGLE;
+			case 4:
+				return VTK_TETRA;
+			case 8:
+				return VTK_HEXAHEDRON;
+			default:
+				if (is_poly)
+					return VTK_POLYHEDRON;
+				if (is_simplex)
+					return VTK_LAGRANGE_TETRAHEDRON;
+				else
+					return VTK_LAGRANGE_HEXAHEDRON;
+			}
+		}
+
+		inline static int VTKTagPlanar(const int n_vertices, bool is_simplex, bool is_poly)
+		{
+			switch (n_vertices)
+			{
+			case 1:
+				return VTK_VERTEX;
+			case 2:
+				return VTK_LINE;
+			case 3:
+				return VTK_TRIANGLE;
+			case 4:
+				return VTK_QUAD;
+			default:
+				if (is_poly)
+					return VTK_POLYGON;
+
+				if (is_simplex)
+					return VTK_LAGRANGE_TRIANGLE;
+				else
+					return VTK_LAGRANGE_QUADRILATERAL;
+			}
+		}
+	};
+
+	class ParaviewWriter
+	{
+	public:
+		ParaviewWriter(){};
+		virtual ~ParaviewWriter(){};
+
+		virtual bool write_mesh(const std::string &path, const Eigen::MatrixXd &points, const Eigen::MatrixXi &cells) = 0;
+		virtual bool write_mesh(const std::string &path, const Eigen::MatrixXd &points, const std::vector<std::vector<int>> &cells, const bool is_simplicial, const bool has_poly) = 0;
+
+		virtual void add_field(const std::string &name, const Eigen::MatrixXd &data) = 0;
+		virtual void add_scalar_field(const std::string &name, const Eigen::MatrixXd &data) = 0;
+		virtual void add_vector_field(const std::string &name, const Eigen::MatrixXd &data) = 0;
+
+		virtual void clear() = 0;
+	};
+} // namespace paraviewo
