ece95f8ed1737fca2ec9ffc875a197a86c6d4a58
Teseo Schneider <teseo@bluewin.ch>
2023-03-21 16:17:41 -0700

    rafactored and enhanced test

--- tests/test.cpp
+++ tests/test.cpp
@@ -9,7 +9,7 @@
 
 using namespace paraviewo;
 
-TEST_CASE("vtu_writer", "[utils]")
+void run_test(ParaviewWriter &writer, const std::string &name)
 {
 	Eigen::MatrixXd pts(25, 3);
 	pts << 0, 0, 0, 0.25, 0, 0, 0, 0.25, 0, 0.25, 0.25, 0,
@@ -24,33 +24,26 @@ TEST_CASE("vtu_writer", "[utils]")
 	Eigen::MatrixXd v(25, 1);
 	v.setRandom();
 
-	Eigen::MatrixXi tris(1, 3);
-	tris << 0, 1, 2;
+	Eigen::MatrixXi tris(2, 3);
+	tris << 0, 1, 2,
+		4, 5, 6;
 
-	VTUWriter writer;
+	Eigen::MatrixXd v_cell(2, 1);
+	v_cell.setRandom();
+
 	writer.add_field("test", v);
-	writer.write_mesh("test.vtu", pts, tris);
+	writer.add_cell_field("ctest", v_cell);
+	writer.write_mesh(name, pts, tris);
+}
+
+TEST_CASE("vtu_writer", "[utils]")
+{
+	VTUWriter writer;
+	run_test(writer, "test.vtu");
 }
 
 TEST_CASE("hdf5_writer", "[utils]")
 {
-	Eigen::MatrixXd pts(25, 3);
-	pts << 0, 0, 0, 0.25, 0, 0, 0, 0.25, 0, 0.25, 0.25, 0,
-		0.5, 0, 0, 0.5, 0.25, 0, 0.75, 0, 0, 0.75, 0.25, 0,
-		1, 0, 0, 1, 0.25, 0, 0, 0.5, 0, 0.25, 0.5, 0,
-		0.5, 0.5, 0, 0.75, 0.5, 0, 1, 0.5, 0, 0, 0.75, 0,
-		0.25, 0.75, 0, 0.5, 0.75, 0, 0.75, 0.75, 0, 1, 0.75, 0,
-		0, 1, 0, 0.25, 1, 0, 0.5, 1, 0, 0.75, 1, 0,
-		1, 1, 0;
-	pts = pts.leftCols(2).eval();
-
-	Eigen::MatrixXd v(25, 1);
-	v.setRandom();
-
-	Eigen::MatrixXi tris(1, 3);
-	tris << 0, 1, 2;
-
 	HDF5VTUWriter writer;
-	writer.add_field("test", v);
-	writer.write_mesh("test.hdf", pts, tris);
+	run_test(writer, "test.hdf");
 }
