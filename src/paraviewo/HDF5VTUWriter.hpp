f02600a508ebc1717109e74f356a6116d01d8fa2
Teseo Schneider <teseo@bluewin.ch>
2023-03-21 16:08:21 -0700

    hdf cell data

--- src/paraviewo/HDF5VTUWriter.hpp
+++ src/paraviewo/HDF5VTUWriter.hpp
@@ -21,12 +21,13 @@ namespace paraviewo
 	{
 
 	public:
-		HDF5VTKDataNode()
+		HDF5VTKDataNode(const bool is_point)
+			: is_point_(is_point)
 		{
 		}
 
-		HDF5VTKDataNode(const std::string &name, const Eigen::MatrixXd &data = Eigen::MatrixXd())
-			: name_(name), data_(data)
+		HDF5VTKDataNode(const bool is_point, const std::string &name, const Eigen::MatrixXd &data = Eigen::MatrixXd())
+			: is_point_(is_point), name_(name), data_(data)
 		{
 		}
 
@@ -40,19 +41,22 @@ namespace paraviewo
 
 		void write(HighFive::File &file) const
 		{
+			const std::string key = is_point_ ? "PointData" : "CellData";
+
 			assert(data_.cols() == 1 || data_.cols() == 3);
 			if (data_.cols() == 3)
-				H5Easy::dump(file, "/VTKHDF/PointData/" + name_, data_);
+				H5Easy::dump(file, "/VTKHDF/" + key + "/" + name_, data_);
 			else
 			{
 				Eigen::Matrix<T, Eigen::Dynamic, 1> tmp = data_.col(0);
-				H5Easy::dump(file, "/VTKHDF/PointData/" + name_, tmp);
+				H5Easy::dump(file, "/VTKHDF/" + key + "/" + name_, tmp);
 			}
 		}
 
 		inline bool empty() const { return data_.size() <= 0; }
 
 	private:
+		const bool is_point_;
 		std::string name_;
 		Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> data_;
 		int n_components_;
@@ -72,15 +76,21 @@ namespace paraviewo
 		void add_scalar_field(const std::string &name, const Eigen::MatrixXd &data) override;
 		void add_vector_field(const std::string &name, const Eigen::MatrixXd &data) override;
 
+		void add_scalar_cell_field(const std::string &name, const Eigen::MatrixXd &data) override;
+		void add_vector_cell_field(const std::string &name, const Eigen::MatrixXd &data) override;
+
 	private:
 		bool is_volume_;
 
 		std::vector<HDF5VTKDataNode<double>> point_data_;
-		std::vector<HDF5VTKDataNode<double>> cell_data_;
 		std::string current_scalar_point_data_;
 		std::string current_vector_point_data_;
 
-		void write_point_data(HighFive::File &file);
+		std::vector<HDF5VTKDataNode<double>> cell_data_;
+		std::string current_scalar_cell_data_;
+		std::string current_vector_cell_data_;
+
+		void write_data(HighFive::File &file);
 		void write_header(const int n_vertices, const int n_elements, HighFive::Group &grp, HighFive::File &file);
 		void write_points(const Eigen::MatrixXd &points, HighFive::File &file);
 		void write_cells(const Eigen::MatrixXi &cells, HighFive::Group &grp, HighFive::File &file);
